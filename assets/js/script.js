dc21b2d974819e61b734f46a37d4eee1e9515c31
mmoore483 <mary.e.moore@hotmail.co.uk>
2021-08-07 15:47:40 +0000

    attempted to implement google charts, not currently working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawChart);
+
+
 /* Following "The Coding Train" YouTube tutorial series Working With Data & APIs in JavScript
 The following function importData retrieves a csv and splits it into rows by linebreak and then 
 into columns by comma
@@ -8,14 +12,38 @@ async function importData() {
     const data = await response.text();
     
     const rows = data.split('\n');
+    const headings = rows[0].split(',');
+
     for (row of rows){
-        const columns = row.split(',');
-        console.log(columns);
+       const columns = parseFloat(row.split(','));
+      
     }
+    
 }
 
-importData()
-.catch(error => {
-            console.log('error!');
-            console.error(error);
-});
+// .catch(error => {
+//             console.log('error!');
+//             console.error(error);
+// });
+
+
+  // Callback that creates and populates a data table,
+  // instantiates the pie chart, passes in the data and
+  // draws it.
+  function drawChart() {
+    importData()
+    // Create the data table.
+    var data = new google.visualization.DataTable();  
+    data.addColumn();
+    data.addRows();
+
+    // Set chart options
+    var options = {'title':'How Much Pizza I Ate Last Night',
+                   'width':400,
+                   'height':300};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.charts.Line(document.getElementById('chart_div'));
+      chart.draw(data, google.charts.Line.convertOptions(options));
+  }
+
